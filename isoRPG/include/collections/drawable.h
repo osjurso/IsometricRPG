70ddd27c1948b2ed77a173f7c8c9768e90616d71
Bjornar Boylestad <bjornb16@student.uia.no>
2017-11-20 10:23:00 +0100

    removed unesesary function

--- isoRPG/include/collections/drawable.h
+++ isoRPG/include/collections/drawable.h
@@ -19,41 +19,6 @@
 class Draweble
 {
 public:
-    void makeDraweble(sf::Texture& texture,sf::Texture& texture1, sf::Texture& texture2, sf::Texture& texture3,int Left, int Top, anax::Entity& entity, std::string state)
-    {
-        entity.addComponent<TextureComponent>();
-        TextureComponent& textureComponent = entity.getComponent<TextureComponent>();
-        textureComponent.tHeroBody = texture;
-        textureComponent.sHeroBody.setTexture(texture);
-        textureComponent.tHeroHead = texture1;
-        textureComponent.sHeroHead.setTexture(texture1);
-        textureComponent.tHeroWeapon = texture2;
-        textureComponent.sHeroWeapon.setTexture(texture2);
-        textureComponent.tHeroShield = texture3;
-        textureComponent.sHeroShield.setTexture(texture3);
-
-        entity.addComponent<PositionComponent>();
-        PositionComponent& positionComponent = entity.getComponent<PositionComponent>();
-        positionComponent.YPos = Top;
-        positionComponent.XPos = Left;
-
-        entity.addComponent<SizeComponent>();
-        SizeComponent& sizeComponent = entity.getComponent<SizeComponent>();
-        sizeComponent.Height = textureComponent.tHeroBody.getSize().x;
-        sizeComponent.Width = textureComponent.tHeroBody.getSize().y;
-        sizeComponent.Height = textureComponent.tHeroHead.getSize().x;
-        sizeComponent.Width = textureComponent.tHeroHead.getSize().y;
-        sizeComponent.Height = textureComponent.tHeroWeapon.getSize().x;
-        sizeComponent.Width = textureComponent.tHeroWeapon.getSize().y;
-        sizeComponent.Height = textureComponent.tHeroShield.getSize().x;
-        sizeComponent.Width = textureComponent.tHeroShield.getSize().y;
-
-        entity.addComponent<StateComponent>();
-        StateComponent& stateComponent = entity.getComponent<StateComponent>();
-        stateComponent.state = state;
-        entity.activate();
-    }
-
     void makeDraweble(sf::Texture& texture,int Left, int Top, anax::Entity& entity, std::string state)
     {
 
